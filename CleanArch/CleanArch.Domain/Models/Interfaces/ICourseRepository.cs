2446654a4ce528e1ac3e587ab46be679771dc5e5
jmabranquinho <jmabranquinho@gmail.com>
2020-03-25 11:25:06 +0000

    Configure Presentation layer for dependency container

---
+++ CleanArch/CleanArch.Domain/Models/Interfaces/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArch.Domain.Models.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Models;
+using CleanArch.Domain.Models.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System.Collections.Generic;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
