2e51326a639705141099afe07695a3695e24f133
jmabranquinho <jmabranquinho@gmail.com>
2020-03-25 15:46:42 +0000

    Changes in Presentation Course controller, views and authentication

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var courseModel = _courseService.GetCourses();
+
+            return View(courseModel);
+        }
+    }
+}
